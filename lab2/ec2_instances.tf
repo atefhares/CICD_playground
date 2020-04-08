524f00ead0bc139afa32d60885b53f8c48122f08
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-04-08 10:36:30 +0200

    launching the 5 ec2s

--- lab2/ec2_instances.tf
+++ lab2/ec2_instances.tf
@@ -1,4 +1,3 @@
-
 resource "aws_instance" "nginx1" {
   ami = "ami-0c322300a1dd5dc79"
   instance_type = "t2.micro"
@@ -7,7 +6,7 @@ resource "aws_instance" "nginx1" {
   tags = {
     Name = "nginx_ec2"
   }
-  vpc_security_group_ids = [aws_security_group.ec2_public_sg.id]
+  vpc_security_group_ids = [aws_security_group.ec2_public_subnet_sg.id]
 }
 
 resource "aws_instance" "nginx2" {
@@ -16,24 +15,69 @@ resource "aws_instance" "nginx2" {
   subnet_id = aws_subnet.publicsubnet2.id
   key_name = aws_key_pair.ec2_key_pair.key_name
   tags = {
-    Name = "nginx_ec2"
+    Name = "nginx2_ec2"
   }
-  vpc_security_group_ids = [aws_security_group.ec2_public_sg.id]
+  vpc_security_group_ids = [aws_security_group.ec2_public_subnet_sg.id]
 }
 
 
-resource "aws_instance" "nginx3" {
+resource "aws_instance" "bastion" {
   ami = "ami-0c322300a1dd5dc79"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.publicsubnet1.id
   key_name = aws_key_pair.ec2_key_pair.key_name
   tags = {
-    Name = "nginx_ec2"
+    Name = "bastion_ec2"
   }
-  vpc_security_group_ids = [aws_security_group.ec2_public_sg.id]
+  vpc_security_group_ids = [aws_security_group.ec2_bastion_sg.id]
 }
 
 output "nginx1_ec2_public_ip" {
   value = aws_instance.nginx1.public_ip
 }
 
+output "nginx2_ec2_public_ip" {
+  value = aws_instance.nginx2.public_ip
+}
+
+output "bastion_ec2_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+//-------------------------------------------------------------------------------------
+
+resource "aws_instance" "gocd" {
+  ami = "ami-0c322300a1dd5dc79"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.publicsubnet1.id
+  key_name = aws_key_pair.ec2_key_pair.key_name
+  tags = {
+    Name = "gocd_ec2"
+  }
+  vpc_security_group_ids = [aws_security_group.ec2_private_subnet_sg.id]
+}
+
+resource "aws_instance" "nexus" {
+  ami = "ami-0c322300a1dd5dc79"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.privatesubnet1.id
+  key_name = aws_key_pair.ec2_key_pair.key_name
+  tags = {
+    Name = "nexus_ec2"
+  }
+  vpc_security_group_ids = [aws_security_group.ec2_private_subnet_sg.id]
+}
+
+output "gocd_ec2_private_ip" {
+  value = aws_instance.gocd.private_ip
+}
+output "gocd_ec2_public_ip" {
+  value = aws_instance.nexus.public_ip
+}
+
+output "nexus_ec2_private_ip" {
+  value = aws_instance.nexus.private_ip
+}
+output "nexus_ec2_public_ip" {
+  value = aws_instance.nexus.public_ip
+}
--- lab2/security_groups.tf
+++ lab2/security_groups.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "ec2_public_sg" {
+resource "aws_security_group" "ec2_public_subnet_sg" {
   name = "public_ec2_sg"
   description = "sg for ec2 machines in the public subnet"
   vpc_id = aws_vpc.mainvpc.id
@@ -7,29 +7,69 @@ resource "aws_security_group" "ec2_public_sg" {
   }
 }
 
-resource "aws_security_group_rule" "ingress_public_ssh_sgr" {
+resource "aws_security_group_rule" "ingress_public_subnet_ssh_sgr" {
   from_port = 22
   to_port = 22
   protocol = "tcp"
   type = "ingress"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_public_sg.id
+  cidr_blocks = [aws_vpc.mainvpc.cidr_block]
+  security_group_id = aws_security_group.ec2_public_subnet_sg.id
 }
 
-resource "aws_security_group_rule" "ingress_public_http_sgr" {
+resource "aws_security_group_rule" "ingress_public_subnet_http_sgr" {
   from_port = 80
   to_port = 80
   protocol = "tcp"
   type = "ingress"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_public_sg.id
+  security_group_id = aws_security_group.ec2_public_subnet_sg.id
 }
 
-resource "aws_security_group_rule" "egress_public_all_tcp_sgr" {
+resource "aws_security_group_rule" "egress_public_subnet_all_tcp_sgr" {
   from_port = 0
   to_port = 65535
   protocol = "tcp"
   type = "egress"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_public_sg.id
+  security_group_id = aws_security_group.ec2_public_subnet_sg.id
+}
+
+//--------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "ec2_bastion_sg" {
+  name = "bastion_ec2_sg"
+  description = "sg for bastion ec2 machine in the public subnet"
+  vpc_id = aws_vpc.mainvpc.id
+  tags = {
+    Name = "bastion_ec2_sg"
+  }
+}
+
+resource "aws_security_group_rule" "ingress_bastion_sg_ssh_sgr" {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  type = "ingress"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_bastion_sg.id
+}
+
+//--------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "ec2_private_subnet_sg" {
+  name = "private_ec2_sg"
+  description = "sg for ec2 machines in the private subnet"
+  vpc_id = aws_vpc.mainvpc.id
+  tags = {
+    Name = "private_ec2_sg"
+  }
+}
+
+resource "aws_security_group_rule" "ingress_private_subnet_ssh_sgr" {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  type = "ingress"
+  cidr_blocks = [aws_vpc.mainvpc.cidr_block]
+  security_group_id = aws_security_group.ec2_private_subnet_sg.id
 }
