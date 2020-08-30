74ab086f52c06fcf96da8d71c2e2f745906f6ff7
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-08-30 20:52:06 +0200

    challenge 6 mods

--- challenge_6/jenkins-deployment.tf
+++ challenge_6/jenkins-deployment.tf
@@ -34,7 +34,7 @@ resource "kubernetes_deployment" "jenkins-deployment" {
           }
 
           volume_mount {
-            name       = kubernetes_persistent_volume.jenkins-volume.metadata[0]
+            name       = kubernetes_persistent_volume.jenkins-volume.metadata[0].name
             mount_path = "/var/jenkins_home"
           }
         }
