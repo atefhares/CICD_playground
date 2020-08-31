c243a25af88523d43c8eab592a9e385fcb8c42c0
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-08-31 16:35:07 +0200

    challenge 6 mods

--- challenge_6/jenkins-deployment.tf
+++ challenge_6/jenkins-deployment.tf
@@ -93,8 +93,7 @@ resource "kubernetes_deployment" "jenkins-deployment" {
         init_container {
           image   = "jenkins/jenkins"
           name    = "init-container-install-docker"
-          # command = ["bash", "-c", "apt update && apt install -y apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' && apt update && apt-get install -y docker-ce docker-ce-cli"]
-          command = ["bash", "-c", "apt update && apt install -y curl && curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh"]
+          command = ["bash", "-c", "apt update && apt install -y apt-transport-https ca-certificates curl software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" && apt update && apt-get install -y docker-ce docker-ce-cli"]
           volume_mount {
             name       = "docker"
             mount_path = "/usr/bin/docker"
